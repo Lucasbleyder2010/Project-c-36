57bdbfd82439a0e0002209e81cdba06f8e52743b
Lucasbleyder2010 <100377923+Lucasbleyder2010@users.noreply.github.com>
2022-07-19 15:52:59 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var canvas, backgroundImage;
+
+var questions;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+  if(keyDown(LEFT_ARROW)){
+    updateHeight(-10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    updateHeight(10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(UP_ARROW)){
+    updateHeight(0,-10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale -0.005;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    updateHeight(0,+10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale+0.005;
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use as setas para mover o balão de ar quente!",40,40);
+ 
+  function updateHeight(x,y){
+    database.ref('balloon/height').update({
+      'x': height.x + x ,
+      'y': height.y + y
+    })
+  }
+  
+  function readHeight(data){
+    height = data.val();
+    console.log(height.x);
+    balloon.x = height.x;
+    balloon.y = height.y;
+   }
+  
+  function showError(){
+    console.log("Erro ao escrever no banco de dados");
+  }
+}
